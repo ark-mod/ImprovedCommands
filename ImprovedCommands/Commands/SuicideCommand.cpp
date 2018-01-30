744eb174a7651fb24d2ef89ea4d6292725963589
Tobias Sebring <tsebring@gmail.com>
2018-01-30 18:30:12 +0100

    Possible fix for suicide command crash.

--- ImprovedCommands/Commands/SuicideCommand.cpp
+++ ImprovedCommands/Commands/SuicideCommand.cpp
@@ -2,11 +2,14 @@
 
 void SuicideChatCommand(AShooterPlayerController* aShooterPlayerController, FString* message, EChatSendMode::Type mode)
 {
+	auto player_character = aShooterPlayerController->GetPlayerCharacter();
+	if (!player_character) return;
+
 	auto cmd = ArkLibrary::GetCommand(CommandName_Suicide_Chat);
 	const uint64 steam_id = ArkApi::IApiUtils::GetSteamIdFromController(aShooterPlayerController);
 	if (cmd && cmd->Permissions == true && !CheckPermission(steam_id, "ImprovedCommands.Suicide", aShooterPlayerController)) return;
 
-	aShooterPlayerController->GetPlayerCharacter()->Suicide();
+	player_character->Suicide();
 
 	ArkApi::GetApiUtils().SendChatMessage(aShooterPlayerController, L"[system]", L"You killed yourself!");
 }
